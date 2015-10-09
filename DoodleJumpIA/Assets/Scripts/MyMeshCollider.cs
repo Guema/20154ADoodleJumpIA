06fb5af6e6f590c16b4b3fd40721a4f538b5bf14
Guema <floorent1993@gmail.com>
2015-10-09 14:56:25 +0200

    myCollider

---
+++ DoodleJumpIA/Assets/Scripts/MyMeshCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMeshCollider : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 position = Vector2.zero;
+    [SerializeField]
+    ColliderType type = ColliderType.Rect;
+    
+	void Update ()
+    {
+	
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(position, 1f);
+    }
+}
